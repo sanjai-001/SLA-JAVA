67b195f40cfb173d02d22eac15484d78d909f51d
sanjai-001 <134257581+sanjai-001@users.noreply.github.com>
2024-11-18 22:37:16 +0530

    Add files via upload

---
+++ looping_statements-18-11-2024/foreach.java
@@ -0,0 +1,13 @@
+package pack;
+
+public class foreach {
+
+	public static void main(String[] args) {
+		int arr[]= {3,5,7};
+		for(int element:arr) {
+			System.out.println("element"+element);
+		}
+
+	}
+
+}
---
+++ looping_statements-18-11-2024/forexample.java
@@ -0,0 +1,14 @@
+package pack;
+
+public class forexample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		for(int i=0;i<n;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
