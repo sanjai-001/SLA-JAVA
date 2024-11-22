4ffe5f9ad776b760a999459d678e84675f8ec042
sanjai-001 <134257581+sanjai-001@users.noreply.github.com>
2024-11-22 16:54:48 +0530

    Add files via upload

---
+++ Pattern/DaimondPattern.java
@@ -0,0 +1,34 @@
+package pack;
+import java.util.*;
+public class DaimondPattern {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++) 
+		{
+			for(int j=1;j<=n-i;j++) 
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		for(int i=n-1;i>0;i--) {
+			for(int j=1;j<=n-i;j++) 
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+
+	}
+
+}
---
+++ Pattern/RightAngleTriangle.java
@@ -0,0 +1,25 @@
+package pack;
+
+import java.util.Scanner;
+
+public class RightAngleTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+//		int number=1;
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=1;j<=i;j++) 
+			{
+				System.out.print(j);
+//				number++;
+			}
+			System.out.println(" ");
+			
+		}
+
+	}
+
+}
