f2ffd45a6ed71376303766b72a4634613d4ef8a3
sanjai-001 <134257581+sanjai-001@users.noreply.github.com>
2024-11-22 16:52:19 +0530

    Add files via upload

---
+++ homework/21-11-2024/Pyramid_One.java
@@ -0,0 +1,21 @@
+package pack;
+
+public class Pyramid_One {
+
+
+		public static void main(String[] args) {
+			int n=1;
+			for(int i=5;i>0;i--) {
+				for(int j=0;j<i;j++) {
+					System.out.print(n);
+					
+				}
+				System.out.println("");
+				n++;
+			}
+
+		}
+
+	}
+
+
