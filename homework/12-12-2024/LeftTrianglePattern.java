d656b56c09893d5c761dd852d86498d044d4d4c4
sanjai-001 <134257581+sanjai-001@users.noreply.github.com>
2024-12-13 20:37:35 +0530

    Add files via upload

---
+++ homework/12-12-2024/LeftTrianglePattern.java
@@ -0,0 +1,22 @@
+package pack;
+import java.util.*;
+public class LeftTrianglePattern {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		int a=sc.nextInt();
+		for(int i=0;i<a;i++) {
+			
+			for(int j=0;j<=i;j++) {
+				
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
